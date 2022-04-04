7293ebfdff4f783eda69c3fcf6614b856ce74b61
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-04-04 21:08:39 +0600

    review responsive probelm fixed

--- src/Component/Review/Review.js
+++ src/Component/Review/Review.js
@@ -5,7 +5,7 @@ const Review = ({comment}) => {
     return (
         <div className="p-8 bg-cyan-200 w-full rounded-lg shadow-lg flex ">
                     <div className=" p-4 bg-gray-100 rounded-lg">
-                        <div className="flex items-center">
+                        <div className="flex flex-col md:flex-row items-center">
                             <img className="h-24 w-24 rounded-full" src={picture} alt="" />
                             <div className="p-3">
                                 {/* <h1 className="text-base font-medium">written by</h1> */}
