a3e89d0cb2ef0c6fb02c974c6021025f504c435a
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-04-04 16:27:37 +0600

    review comment and image added and blogs design improved

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -8,16 +8,16 @@ const Blogs = () => {
                     <h1 class='mb-6 text-xl font-blod text-gray-900 md:text-4xl'>
                         Questions and Answer
                     </h1>
-                    <p class='mt-10 mb-3 font-semibold text-gray-900'>
+                    <p class='mt-10 mb-3 font-semibold text-gray-900 text-xl'>
                         what is  React Context API ?
                     </p>
                     <p class='text-gray-600'>
                         we know that react is better for it profomance. The one secrets is one Understand one-way data bindings. But in here some problem are occur line if we though data in route and subroute component then all component are need to <strong>"Prop Drilling"</strong>.And it not effecient. So, solve this problem they have Context API in react.
                         <br />
-                        The React Context API is a way for a React app to effectively produce global variables that can be passed around.Context is designed to share data that can be considered “global” for a tree of React components and then in side router components can easily acesses.
+                        The React Context API is a way for a React app to effectively produce global variables that can be passed around.Context is designed to share data that can be considered <strong>“global” for a tree of React components </strong>and then in side router components can easily acesses.
                     </p>
                     <p class='mt-10 mb-3 font-semibold text-gray-900 text-xl'>
-                        what is symantic tag ??
+                        what is symantic tag ?
                     </p>
                     <p class='text-gray-600'>
                         In HTML symantic tag clearly describles its meaning. There are have symantic and non-symantic tag. at first looking "Non-symantic" tag are not to understandable what content took it.
--- src/Component/Review/Review.js
+++ src/Component/Review/Review.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Review = ({comment}) => {
-    const {name,email,comments,rating} =comment;
+    const {name,email,comments,rating,picture} =comment;
     return (
         <div className="p-8 bg-cyan-200 w-full rounded-lg shadow-lg flex ">
                     <div className=" p-4 bg-gray-100 rounded-lg">
                         <div className="flex items-center">
-                            <img className="h-24 w-24 rounded-full" src="https://i.ibb.co/xftQCp9/recs-attackontitan2-removebg-preview.png" alt="" />
+                            <img className="h-24 w-24 rounded-full" src={picture} alt="" />
                             <div className="p-3">
-                                <h1 className="text-base font-medium">written by</h1>
+                                {/* <h1 className="text-base font-medium">written by</h1> */}
                                 <h1 className=" text-base font-medium"><span className='text-xl font-bold'>{name}</span> || <span className="underline underline-offset-1">{email}</span></h1>
                                 <h1 className="text-base ">rating: <span className="font-bold">{rating}</span></h1>
                             </div>
